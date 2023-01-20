df65d3335824f7b25ab5b15573874404e87c8c42
devgenx <sebastian110420@gmail.com>
2023-01-19 19:56:58 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -1,44 +1,61 @@
-// write your code here
-// details
-const ramenMenuImg = document.getElementById("ramen-menu");
+// ramen details
+const ramenName = document.querySelector(".name");
+const ramenResto = document.querySelector(".restaurant");
 const ramenImage = document.querySelector(".detail-image");
-const ramenDetail = document.getElementById("ramen-detail");
+// ramen form rating and comment
+const rating = document.getElementById("rating-display");
+const comments = document.getElementById("comment-display");
 
-// name and restaurant
+// ramen form and menu
+const ramenMenu = document.getElementById("ramen-menu");
+const ramenForm = document.querySelector("#new-ramen");
 
-// button and form
+const deleteBtn = document.getElementById("delete");
+
+let dataList = [];
 
 const fetchData = async () => {
-  const response = await fetch("http://localhost:3000/ramens");
-
-  const data = await response.json();
-  data.forEach((item) => handleRamenImg(item));
+  try {
+    const response = await fetch("http://localhost:3000/ramens");
+    dataList = await response.json();
+    dataList.forEach((item) => handleRamenData(item));
+    renderData(dataList[0]);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const handleRamenImg = (data) => {
-  const ramenName = document.querySelector(".name");
-  const ramenResto = document.querySelector(".restaurant");
+// create image for every ramen, set src as the image on db.json
 
-  //rating and comments
-  const rating = document.getElementById("rating-display");
-  const comments = document.getElementById("comment-display");
+// append the images into the ramen menu section
+
+const handleRamenData = (data) => {
   const ImagesRamen = document.createElement("img");
   ImagesRamen.src = data.image;
 
-  ImagesRamen.addEventListener("click", (e) => {
-    ramenImage.src = data.image;
-    ramenName.textContent = data.name;
-    rating.textContent = data.rating;
-    comments.textContent = data.comment;
-    ramenResto.textContent = data.restaurant;
+  ImagesRamen.addEventListener("click", () => {
+    renderData(data);
   });
-  ramenMenuImg.appendChild(ImagesRamen);
+  ramenMenu.appendChild(ImagesRamen);
 };
 
-const ramenForm = document.querySelector("#new-ramen");
+// render the data and assign the values to be the values from the database
+// ramen image is the image in the middle
+const renderData = (data) => {
+  ramenImage.src = data.image;
+  ramenName.textContent = data.name;
+  rating.textContent = data.rating;
+  comments.textContent = data.comment;
+  ramenResto.textContent = data.restaurant;
+  deleteBtn.textContent = "x";
+
+  //deleteBtn.id = data.id;
+};
+// set the values of the ramen form
+// get the values and input them in the object
+// call the handleRamenData to the data in the object to assign the values of every new input ramen
 
 ramenForm.addEventListener("submit", (e) => {
-  console.log("form-submitted");
   e.preventDefault();
   const newName = document.querySelector("#new-name").value;
   const newResto = document.querySelector("#new-restaurant").value;
@@ -53,9 +70,33 @@ ramenForm.addEventListener("submit", (e) => {
     rating: newRating,
     comment: newComment,
   };
-
-  handleRamenImg(ramen);
+  handleRamenData(ramen);
   ramenForm.reset();
 });
 
+const setRating = async (id) => {
+  const currData = dataList.find((data) => data.id === id);
+  //console.log(currData);
+  const config = {
+    method: "PATCH",
+    body: JSON.stringify({
+      rating: currData.rating,
+    }),
+  };
+
+  await fetch(`http://localhost:3000/ramens/${id}`, config);
+};
+
+const updateForm = document.querySelector("#update-ramen");
+
+updateForm.addEventListener("submit", (e) => {
+  const setNewRating = document.querySelector("#set-rating").value;
+  e.preventDefault();
+  setRating(setNewRating, dataList.id);
+});
+
 fetchData();
+
+deleteBtn.addEventListener("click", (e) => {
+  e.target.remove();
+});
